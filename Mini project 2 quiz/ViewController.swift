2284119a788bc85265c19a3323b10b591422930f
AmberNguyen249 <ambernguyen2017@gmail.com>
2022-06-14 13:27:29 -0400

    Initial Commit

---
+++ Mini project 2 quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini project 2 quiz
+//
+//  Created by Amber Nguyen on 6/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
